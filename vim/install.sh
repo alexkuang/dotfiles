25e03bf0c890bde4b933f46c4c4deb2c06a2edaf
Alex Kuang <akuang@linkedin.com>
2014-11-24 14:44:05 -0500

    Get rid of vim-sneak

--- vim/install.sh
+++ vim/install.sh
@@ -12,8 +12,8 @@ fi
 pathogen_dir="$HOME/.vim/bundle"
 pathogen_modules=('kien/ctrlp.vim' 'derekwyatt/vim-scala' 'kien/rainbow_parentheses.vim' 'tomtom/tlib_vim'
 'MarcWeber/vim-addon-mw-utils' 'tpope/vim-dispatch' 'tpope/vim-surround' 'tpope/vim-repeat' 'godlygeek/tabular'
-'tpope/vim-abolish' 'tpope/vim-commentary' 'justinmk/vim-sneak' 'tommcdo/vim-exchange' 'SirVer/ultisnips'
-'wesgibbs/vim-irblack' 'elixir-lang/vim-elixir')
+'tpope/vim-abolish' 'tpope/vim-commentary' 'tommcdo/vim-exchange' 'SirVer/ultisnips' 'wesgibbs/vim-irblack'
+'elixir-lang/vim-elixir')
 
 echo '[vim] Installing pathogen modules...'
 for module in "${pathogen_modules[@]}"
