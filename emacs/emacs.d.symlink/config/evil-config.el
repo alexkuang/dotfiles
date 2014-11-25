c7ebbfb5dae843ccbad75a613ac5c9f8e339f0c0
Alex Kuang <akuang@linkedin.com>
2014-11-25 07:29:31 -0500

    Set evil shift width before activating evil-mode, oops

--- emacs/emacs.d.symlink/config/evil-config.el
+++ emacs/emacs.d.symlink/config/evil-config.el
@@ -5,6 +5,8 @@
 (require 'evil-search-highlight-persist)
 (require 'evil-surround)
 
+(setq evil-shift-width 2)
+
 (key-chord-mode t)
 (global-evil-leader-mode t)
 (global-evil-surround-mode t)
@@ -15,7 +17,5 @@
 (evil-leader/set-key "h" 'evil-search-highlight-persist-remove-all)
 (key-chord-define evil-insert-state-map  "jk" 'evil-normal-state) ; inoremap jk <ESC>
 (evil-mode t)
-
-(setq evil-shift-width 2)
 
 (provide 'evil-config)
