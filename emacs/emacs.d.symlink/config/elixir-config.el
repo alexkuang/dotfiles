fb32df88c4792ebc2c309a00fb2f6e8bed43a563
Alex Kuang <alex.kuang.13@gmail.com>
2014-12-10 07:13:08 -0500

    Some settings updates for elixir

--- emacs/emacs.d.symlink/config/elixir-config.el
+++ emacs/emacs.d.symlink/config/elixir-config.el
@@ -2,7 +2,9 @@
 
 (autoload 'elixir-mode "elixir-mode" "Major mode for elixir" t)
 (add-to-list 'auto-mode-alist '("\\.elixir2\\'" . elixir-mode)) ; the defaults should be included already
-(add-hook 'elixir-mode-hook '(lambda () (alchemist-mode t)))
+(add-hook 'elixir-mode-hook '(lambda ()
+                               (setq evil-shift-width 2)
+                               (alchemist-mode t)))
 
 (provide 'elixir-config)
 
