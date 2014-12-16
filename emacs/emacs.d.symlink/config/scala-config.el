82267a1235f5ae624f33f94c71d1a9ee5de1a6f1
Alex Kuang <akuang@linkedin.com>
2014-12-16 08:11:08 -0500

    Set shiftwidth=2 for scala

--- emacs/emacs.d.symlink/config/scala-config.el
+++ emacs/emacs.d.symlink/config/scala-config.el
@@ -3,4 +3,7 @@
 (autoload 'scala-mode "scala-mode2" "Major mode for scala" t)
 (add-to-list 'auto-mode-alist '("\\.scala\\'" . scala-mode))
 
+(add-hook 'scala-mode-hook '(lambda ()
+                            (setq evil-shift-width 2)))
+
 (provide 'scala-config)
