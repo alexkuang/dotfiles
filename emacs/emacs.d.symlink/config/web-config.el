3f70f25f7522c353fdc67436698a06016dc1f34f
Alex Kuang <akuang@linkedin.com>
2015-02-20 07:50:25 -0500

    Fix indentation in web-mode

--- emacs/emacs.d.symlink/config/web-config.el
+++ emacs/emacs.d.symlink/config/web-config.el
@@ -3,4 +3,11 @@
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
+(defun web-mode-config-hook ()
+  "Configure web-mode on load"
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+
+(add-hook 'web-mode-hook 'web-mode-config-hook)
+
 (provide 'web-config)
