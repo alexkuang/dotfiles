fb4797b5cfa7ea058a725cdb4eae94573c2e448b
Alex Kuang <akuang@linkedin.com>
2015-03-16 10:48:02 -0700

    Fix scala indenting, finally

--- emacs/emacs.d.symlink/config/scala-config.el
+++ emacs/emacs.d.symlink/config/scala-config.el
@@ -4,6 +4,7 @@
 (add-to-list 'auto-mode-alist '("\\.scala\\'" . scala-mode))
 
 (add-hook 'scala-mode-hook '(lambda ()
-                            (setq evil-shift-width 2)))
+                            (setq evil-shift-width 2
+                                  scala-indent:use-javadoc-style t)))
 
 (provide 'scala-config)
