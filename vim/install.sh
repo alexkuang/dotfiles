beb7bf29f950377f98bad37475d97dcf4e8f2008
Alex Kuang <alex.kuang.13@gmail.com>
2017-12-02 11:34:28 -0500

    bootstrap minpac packages; change rainbow plugi

--- vim/install.sh
+++ vim/install.sh
@@ -5,6 +5,7 @@ then
     echo '[vim] Installing minpac...'
     mkdir -p ~/.vim/pack/minpac/opt
     git clone https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac
+    vim "+packadd minpac | source ~/.vim/vimrc | call minpac#update('', {'do': 'quit'})"
 fi
 
 exit 0
