44320f77dfab8af33c65420636ef4d3ea7bd7053
Alex Kuang <alex.kuang.13@gmail.com>
2020-03-20 20:02:48 -0400

    asdf configs

---
+++ asdf/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# set up asdf plugins and such
+
+asdf plugin-add python https://github.com/danhper/asdf-python.git
+
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+asdf plugin-add terraform https://github.com/Banno/asdf-hashicorp.git
+
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
