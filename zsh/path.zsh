865032a89a57f008c6502d68c8659e960598b40b
alex <alex.kuang.13@gmail.com>
2023-01-27 20:49:29 -0500

    apparently homebrew changed location, or something

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,5 +1,5 @@
 export PATH=/usr/local/share/npm/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:~/bin
 
-export PATH="$PATH:$HOME/.dotfiles/bin"
+export PATH="$PATH:$HOME/.dotfiles/bin:/opt/homebrew/bin"
 
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:$PATH"
