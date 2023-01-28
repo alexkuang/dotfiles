225946e15bb9c486842bb11be6a2df5fd382dcf6
alex <alex.kuang.13@gmail.com>
2023-01-28 10:44:45 -0500

    more brew path fixes

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,7 +1,7 @@
+
 export PATH=/usr/local/share/npm/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:~/bin
 
 export PATH="$PATH:$HOME/.dotfiles/bin:/opt/homebrew/bin"
 
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:$PATH"
-
 eval "$(brew shellenv)"
+export PATH="${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin:${HOMEBREW_PREFIX}/opt/findutils/libexec/gnubin:$PATH"
