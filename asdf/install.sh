445c6cdfe8b0dca45233b609dcb4525ed3efb73e
alex <alex.kuang.13@gmail.com>
2023-01-27 20:50:01 -0500

    add asdf elixir

--- asdf/install.sh
+++ asdf/install.sh
@@ -11,4 +11,7 @@ asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin-add terraform https://github.com/Banno/asdf-hashicorp.git
 
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
