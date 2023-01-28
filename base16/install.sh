62fc352eff3738ffdd92112bc230c8086b03bde1
alex <alex.kuang.13@gmail.com>
2023-01-28 07:01:04 -0500

    update base16 setup

--- base16/install.sh
+++ base16/install.sh
@@ -1,5 +1,9 @@
 # Support for 256 colors using base16 themes
 
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-base16_default-dark
 
+BASE16_SHELL=$HOME/.config/base16-shell/
+[ -n "$PS1" ] && \
+        [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+                source "$BASE16_SHELL/profile_helper.sh"
+base16_default-dark
