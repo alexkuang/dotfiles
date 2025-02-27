e6650be98f7e8f965d38efc9b22b4ca45cbfe0a0
alex <alex.kuang.13@gmail.com>
2025-02-27 12:43:19 -0500

    update asdf install script

--- asdf/install.sh
+++ asdf/install.sh
@@ -2,16 +2,12 @@
 #
 # set up asdf plugins and such
 
-echo -e "\n. $(brew --prefix asdf)/asdf.sh" >> ~/.zshrc
+asdf plugin add python https://github.com/danhper/asdf-python.git
 
-asdf plugin-add python https://github.com/danhper/asdf-python.git
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 
-asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin add terraform https://github.com/Banno/asdf-hashicorp.git
 
-asdf plugin-add terraform https://github.com/Banno/asdf-hashicorp.git
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
-asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-
-asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-
-bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
