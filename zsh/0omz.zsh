0824971da46d6e5a16ae9412b259ead0dfce2db3
Alex Kuang <alex@bikeshed.coffee>
2025-07-01 10:39:02 -0400

    direnv

--- zsh/0omz.zsh
+++ zsh/0omz.zsh
@@ -37,6 +37,6 @@ DISABLE_AUTO_TITLE="true"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 zstyle ':omz:plugins:nvm' lazy yes
-plugins=(nvm git)
+plugins=(nvm git direnv)
 
 source $ZSH/oh-my-zsh.sh
